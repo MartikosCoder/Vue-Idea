90120cbb35c747c743c3451a7c853323ed903a15
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2019-10-20 20:37:59 +0300

    IMPROVE: change toFixed from 2 to 4

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -27,8 +27,8 @@ export default {
         const _this = this;
         _this.watchCoords = setInterval(() => {
           navigator.geolocation.getCurrentPosition(position => {
-            _this.$set(_this, 'latitude', position.coords.latitude.toFixed(2));
-            _this.$set(_this, 'longitude', position.coords.longitude.toFixed(2));
+            _this.$set(_this, 'latitude', position.coords.latitude.toFixed(4));
+            _this.$set(_this, 'longitude', position.coords.longitude.toFixed(4));
           });
         }, 1000);
       }
