f3f695eee34d12450ffde0824edfa4b4cc75afca
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2019-10-20 20:05:44 +0300

    Get coords

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="main">
     {{ msg }}
+    <div class="latitude">Latitude: {{ latitude }}</div>
+    <div class="longitude">Longitude: {{ longitude }}</div>
   </div>
 </template>
 
@@ -9,6 +11,23 @@ export default {
   name: 'Main',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      latitude: 0,
+      longitude: 0
+    }
+  },
+  methods: {
+    getCoords() {
+      if (navigator.geolocation) {
+        const _this = this;
+        navigator.geolocation.getCurrentPosition(position => {
+          _this.latitude = position.coords.latitude;
+          _this.longitude = position.coords.longitude;
+        });
+      }
+    }
   }
 }
 </script>
